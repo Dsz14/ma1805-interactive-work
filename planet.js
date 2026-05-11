a342f495703a2a78b251a9a4c923ffecc24cd3b3
Dsz14 <daniel14.genesis@gmail.com>
2026-05-11 22:05:23 +0100

    Add files via upload

--- planet.js
+++ planet.js
@@ -1,25 +1,25 @@
-class Planet {
-    constructor(x, y, mass) {
-        this.pos = createVector(x, y);
-        this.vel = p5.Vector.random2D();
+class Planet { // the planet class is used to create the planet and update its position based on the forces applied to it
+    constructor(x, y, mass) { // the constructor takes in the x and y position of the planet and its mass
+        this.pos = createVector(x, y); // the position of the planet is stored as a p5 vector
+        this.vel = p5.Vector.random2D(); // the velocity of the planet is initialized to a random direction 
         this.vel.mult(5);
         this.acc = createVector(0, 0);
         this.mass = mass;
         this.color = [random(255), random(255), random(255)];
     }
 
-applyForce(force) {
-    let f = p5.Vector.div(force, this.mass);
+applyForce(force) { // the applyForce function takes in a force vector and applies it to the planet by adding it to the acceleration
+    let f = p5.Vector.div(force, this.mass); // the force is divided by the mass of the planet to get the acceleration (F=ma)
     this.acc.add(f);
 }
 
-update() {
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
+update() { // the update function updates the velocity and position of the planet based on the acceleration
+    this.vel.add(this.acc); // the velocity is updated by adding the acceleration to it
+    this.pos.add(this.vel); // the position is updated by adding the velocity to it
     this.acc.set(0, 0);
 }
 
-show() {
+show() { // the show function is used to display the planet on the canvas
     stroke("white");
     strokeWeight(2);
     fill(this.color);
--- sun.js
+++ sun.js
@@ -1,25 +1,25 @@
-class Sun { 
-    constructor(x, y, m) {
+class Sun { // the sun class is used to create the sun and calculate the gravitational force it exerts on the planet
+    constructor(x, y, m) { // the constructor takes in the x and y position of the sun and its mass
 
-        this.pos = createVector(x, y);
-        this.mass = m;
+        this.pos = createVector(x, y); // the position of the sun is stored as a p5 vector
+        this.mass = m; // the mass of the sun is stored as a variable
 
 }
 
-attract(planet) {
-    let force = p5.Vector.sub(this.pos, planet.pos);
-    let distanceSq = constrain(force.magSq(), 25, 2500);
+attract(planet) { // the attract function takes in a planet object and calculates the gravitational force it exerts on the planet
+    let force = p5.Vector.sub(this.pos, planet.pos);// the force is calculated as the vector from the planet to the sun
+    let distanceSq = constrain(force.magSq(), 25, 2500);// the distance squared is calculated and constrained to prevent extreme forces when the planet is too close or too far from the sun
     let G=10;
     let strength = G * (this.mass * planet.mass) / distanceSq;
 
-    force.setMag(strength);
-    planet.applyForce(force);
+    force.setMag(strength); // the force vector is set to have the magnitude of the strength of the gravitational force
+    planet.applyForce(force); // the force is applied to the planet using the applyForce function in the Planet class
 }
 
-show () {
+show () { // the show function is used to display the sun on the canvas
     noStroke();
     fill("yellow");
     this.r = sqrt(this.mass) * 10 * scale; // radius is based on the mass of the sun and the scale
-    ellipse(this.pos.x*scale, this.pos.y*scale, this.r * 2*scale);
+    ellipse(this.pos.x*scale, this.pos.y*scale, this.r * 2*scale);// the sun is drawn as a yellow circle with a radius based on its mass and the scale
 }
 }
